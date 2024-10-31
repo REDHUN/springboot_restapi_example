f41f27edc84b796c5f44dd6ba7bc005879a0dfec
Redhun <redhuns123@gmail.com>
2024-10-31 20:56:54 +0530

    jpa added and connected with database

--- src/main/java/com/redhun/spring_boot_rest/repo/JobRepo.java
+++ src/main/java/com/redhun/spring_boot_rest/repo/JobRepo.java
@@ -2,6 +2,7 @@ package com.redhun.spring_boot_rest.repo;
 
 
 import com.redhun.spring_boot_rest.model.JobPost;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,61 +10,64 @@ import java.util.Arrays;
 import java.util.List;
 
 @Repository
-public class JobRepo {
+public interface JobRepo extends JpaRepository<JobPost,Integer> {
 
 
-    List<JobPost> jobs = new ArrayList<>(Arrays.asList(
-
-            new JobPost(1, "java dev", "good exp", 2, List.of("Redhun", "kichun")),
-            new JobPost(2, "java dev", "good exp", 2, List.of("heeee", "j"))
-    ));
-
-    public List<JobPost> getAllJobs() {
-
-        return jobs;
-    }
-
-    public void addJob(JobPost post) {
-        jobs.add(post);
-        System.out.println(jobs);
-    }
+    List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile,String postDesc);
 
 
-    public JobPost getJob(int postId) {
-
-        for (JobPost job : jobs) {
-            if (job.getPostId() == postId) {
-                return job;
-            }
-        }
-        return null;
-
-
-    }
-
-    public void updateJob(JobPost jobPost) {
-        for (JobPost job : jobs) {
-
-            if (job.getPostId() == jobPost.getPostId()) {
-
-                job.setPostProfile(jobPost.getPostProfile());
-                job.setPostDesc(jobPost.getPostDesc());
-                job.setReqExperience(jobPost.getReqExperience());
-                job.setPostTechStack(jobPost.getPostTechStack());
-            }
-        }
-
-
-    }
-
-    public void deleteJob(int postId) {
-
-        for (JobPost job : jobs) {
-
-            if (job.getPostId() == postId) {
-                jobs.remove(job);
-
-            }
-        }
-    }
 }
+//List<JobPost> jobs = new ArrayList<>(Arrays.asList(
+//
+//        new JobPost(1, "java dev", "good exp", 2, List.of("Redhun", "kichun")),
+//        new JobPost(2, "java dev", "good exp", 2, List.of("heeee", "j"))
+//));
+//
+//public List<JobPost> getAllJobs() {
+//
+//    return jobs;
+//}
+//
+//public void addJob(JobPost post) {
+//    jobs.add(post);
+//    System.out.println(jobs);
+//}
+//
+//
+//public JobPost getJob(int postId) {
+//
+//    for (JobPost job : jobs) {
+//        if (job.getPostId() == postId) {
+//            return job;
+//        }
+//    }
+//    return null;
+//
+//
+//}
+//
+//public void updateJob(JobPost jobPost) {
+//    for (JobPost job : jobs) {
+//
+//        if (job.getPostId() == jobPost.getPostId()) {
+//
+//            job.setPostProfile(jobPost.getPostProfile());
+//            job.setPostDesc(jobPost.getPostDesc());
+//            job.setReqExperience(jobPost.getReqExperience());
+//            job.setPostTechStack(jobPost.getPostTechStack());
+//        }
+//    }
+//
+//
+//}
+//
+//public void deleteJob(int postId) {
+//
+//    for (JobPost job : jobs) {
+//
+//        if (job.getPostId() == postId) {
+//            jobs.remove(job);
+//
+//        }
+//    }
+//}
